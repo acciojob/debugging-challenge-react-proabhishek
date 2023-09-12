74a04bd6bf544dc36a283752dec3d584cb62920b
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-09-12 22:30:33 +0530

    attempt2

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ const App = () => {
 
   return (
     <div className="ball">
-      <h1 className="count" onClick={() => { alert("cant edit it") }}>{count}</h1>
-      <button className='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
+      <h1 className="count" onClick={() => { alert("get Lost") }}>{count}</h1>
+      <button className='increment-button' onClick={() => { setCount(0) }}>Increment</button>
     </div>
   )
 }
